16d6ae900d5e6ce4d83205d013ffc92d6ecca769
Dmitry Valter <dvalter@protonmail.com>
2020-03-21 01:27:25 +0300

    Fix NPE in PreviewFragment
    Catch NPE in GradientDrawable::getCornerRadii existing due to
    poor SDK implementation and incorrect documentation.
    
    Change-Id: I2cd5e2f398c4f7ea30493888e1029ea8076523ee

--- src/com/android/wallpaper/picker/PreviewFragment.java
+++ src/com/android/wallpaper/picker/PreviewFragment.java
@@ -227,12 +227,16 @@ public abstract class PreviewFragment extends Fragment implements
         // Workaround as we don't have access to bottomDialogCornerRadius, mBottomSheet radii are
         // set to dialogCornerRadius by default.
         GradientDrawable bottomSheetBackground = (GradientDrawable) mBottomSheet.getBackground();
-        float[] radii = bottomSheetBackground.getCornerRadii();
-        for (int i = 0; i < radii.length; i++) {
-            radii[i]*=2f;
+        try {
+            float[] radii = bottomSheetBackground.getCornerRadii();
+            for (int i = 0; i < radii.length; i++) {
+                radii[i]*=2f;
+            }
+            bottomSheetBackground = ((GradientDrawable)bottomSheetBackground.mutate());
+            bottomSheetBackground.setCornerRadii(radii);
+        } catch (NullPointerException npe) {
+           // Log.e(TAG, "cornerRadii not available", npe);
         }
-        bottomSheetBackground = ((GradientDrawable)bottomSheetBackground.mutate());
-        bottomSheetBackground.setCornerRadii(radii);
         mBottomSheet.setBackground(bottomSheetBackground);
 
         mBottomSheetInitialState = (savedInstanceState == null) ? STATE_EXPANDED
